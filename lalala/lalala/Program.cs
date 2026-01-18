5e3ddc03095179885653c6a9152b5ad0464ccc51
Frogmaster_Pepe <pepe.rotscidl@gmail.com>
2026-01-18 19:15:48 +0100

    nejaký random úpravy co se děly radši sem to stagenul samostatně

---
+++ lalala/lalala/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace lalala
+{
+    internal class Program
+    {
+        //struct Hrana
+       // {
+           // public int Start;
+           // public int Cil; 
+           // public int Len; 
+            //public bool Placeni;
+
+           // public Hrana(int Start,int Cil,int Len, int placena) 
+                //{
+                //Start = start;
+               // Cil = cil;
+               // Len = len;
+                //if (placena == 1)
+               // {
+               //     Placeni = true;
+                //}
+               // else
+               // {
+                   // Placeni = false;
+                //}
+
+            //}
+       // }
+        static void Main(string[] args)
+        {
+           string blem = Console.ReadLine();
+           string[] mlem = blem.Split();
+            int vrch = Convert.ToInt32(mlem[0]);
+            int hranc = Convert.ToInt32(mlem[1]);
+            for(int i = 0; i < hranc; i++);
+            {
+                string udaj = Console.ReadLine();
+                string[] plop = udaj.Split();
+                int start = Convert.ToInt32(plop[0]);
+                int cil
+            }
+        }
+    }
+}
