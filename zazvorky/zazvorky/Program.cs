aaf8f78f0fd2c5f6dced989856658c915c9d4a32
Frogmaster_Pepe <pepe.rotscidl@gmail.com>
2026-01-18 20:25:15 +0100

    zavorky

---
+++ zazvorky/zazvorky/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace zazvorky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpravneZavorky();
+        }
+       
+        static bool SpravneZavorky()
+        {
+                Console.Write("Pro ukončení programu zadej - k; jinak zadej závorky, které chceš zkontrolovat: ");
+                string zavorky = Console.ReadLine();
+                Stack<char> seznam = new Stack<char>();
+                var páry = new Dictionary<char, char>
+                {
+                    { ')', '(' },
+                    { ']', '[' },
+                    { '}', '{' }
+                };
+                
+                foreach (char znak in zavorky)
+                {
+                    if ("([{".Contains(znak))
+                    {
+                        seznam.Push(znak);
+                    }
+                    else if (")]}".Contains(znak))
+                    {
+                        if (seznam.Count == 0 || seznam.Peek() != páry[znak])
+                        {
+                            Console.WriteLine("Špatně");
+                            return false;
+                        }
+                        seznam.Pop();
+                    }
+                }
+                Console.WriteLine("Správně");
+                return true;
+        }
+    }
+}
+
+
+
+
