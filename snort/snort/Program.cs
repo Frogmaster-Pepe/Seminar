30f7b27b14841261e91e598546c6adc2d657f0a2
Frogmaster_Pepe <pepe.rotscidl@gmail.com>
2026-03-16 15:51:47 +0100

    mlem

---
+++ snort/snort/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace snort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla = {2,6,3,7,9,1,5};
+            int l = cisla.Length;
+            for(int i = 0; i < l; i++)
+            {
+                for(int j = 0; i < l - 1; j++)
+                {
+                    if (cisla[j] > cisla[j+1])
+                    {
+                        int x = cisla[j];
+                        cisla[j] = cisla[j = 1];
+                        cisla[j = 1] = x;
+                    }
+                }
+            }
+            for (int i = 0; i < l; i++)
+            { 
+                Console.WriteLine(cisla[i]);
+            }
+        }
+    }
+}
