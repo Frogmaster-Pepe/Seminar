c3fcd066bfe9d11bd996efdb07f33a8bcde0e454
Frogmaster_Pepe <pepe.rotscidl@gmail.com>
2026-03-17 17:59:50 +0100

    testik
    aaaahhhh

---
+++ Testik_2/Testik_2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Testik_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int vstup = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i<vstup;i++)
+            {
+                string[] vstupy = Console.ReadLine().Split();
+                int[] cisla = new int[vstupy.Length];
+                for (int j = 0; j < vstupy.Length; j++)
+                {
+                    cisla[j] = Convert.ToInt32(vstupy[j]);
+                }
+            }
+            }
+            
+            static void vypisGrafu()
+            {
+                for (int i = 0; i < cisla.Length; i++)
+                {
+                    for (int j = 0; i < cisla.Length; i++)
+                    {
+
+                    }
+                }
+            }
+
+
+
+
+
+
+                  
+        
+    }
+}
